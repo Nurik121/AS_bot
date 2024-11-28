70591987485d4783e464109e019fec688a97d218
Овчинников Максим <ovchinnikow.37@gmail.com>
2024-11-28 13:30:09 +0600

    Очистка конфига и удаление функций, связанных с генерацией ответа от бота

--- src/packages/bot/chat_bot.py
+++ src/packages/bot/chat_bot.py
@@ -32,49 +32,9 @@ class ChatBot:
         self._logger = logger
 
     def generate_answer(self, text: str) -> str:
-        """
-        Initialize ChatBot class with logger.
-        @param text: some question from user.
-        @return: return user text.
-        """
-        intent = self._get_question_intent(text)
-        self._log_question(text, intent)
-
-        if intent is not None:
-            return self._get_random_answer_by_intent(intent)
-        return self._get_reserve_answer()
-
-    @staticmethod
-    def _get_random_answer_by_intent(intent: str) -> str:
-        """
-        Getting random answer for specific intent.
-        @param intent: intent for user question.
-        @return: random selected response for question.
-        """
-        responses = config["intents"][intent]["responses"]
-        return random.choice(responses)
-
-    @staticmethod
-    def _get_reserve_answer() -> str:
-        """
-        Getting reserve answer, return failure phrase from `bot_config`.
-        @return: string with reserve answer.
-        """
-        answer = random.choice(config["failure_phrases"])
+        answer = config["start_phrase"]
+        self._log_question(text, answer)
         return answer
-
-    def _get_question_intent(self, text: str) -> str or None:
-        """
-        Getting intent for user question.
-        @param text: some question from user.
-        @return: some intent for question.
-        """
-        # try:
-        return None #self._chat_model.get_intent(text)
-        # pylint: disable=W0703:
-        #except Exception as exception:
-        #     ChatBotException(f"Unexpected error: {exception}")
-        #     return None
 
     def _log_question(self, text: str, intent: str) -> None:
         """
